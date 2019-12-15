a304bf356fc3a3f5ab0fa8072deabbaad741edd2
da-okazaki <da-okazaki@outlook.com>
2019-12-15 14:44:35 +0900

    [update] react-apollo-app Auth情報追加

--- react-apollo-client/src/index.js
+++ react-apollo-client/src/index.js
@@ -4,6 +4,46 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+
+
+const client = new ApolloClient({
+    uri: 'https://api.github.com/graphql',
+    request: operation => {
+      operation.setContext({
+        headers: {
+          authorization: `Bearer ${process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN}`
+        }
+      });
+    }
+  });
+  
+  const query = gql`
+    {
+      organization(login: "apollographql") {
+        repositories(first: 5) {
+          nodes {
+            id
+            name
+            url
+            viewerHasStarred
+            stargazers {
+              totalCount
+            }
+          }
+        }
+      }
+    }
+  `
+  
+  client
+    .query({
+      query
+    })
+    .then(result => console.log(result));
+
+    
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
